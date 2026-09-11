d19f42ae498a930f55183aebfe2bde2896f2da8c
Gabriel Zambo <gabrieltzmb@gmail.com>
2026-09-11 12:18:24 -0300

    feat: adiciona persistência com localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import TaskForm from "./components/TaskForm"
 import TaskList from "./components/TaskList"
 import FilterButtons from "./components/FilterButtons"
@@ -7,6 +7,23 @@ import FilterButtons from "./components/FilterButtons"
 const App = () => {
   const [tarefas, setTarefas] = useState([])
   const [filtro, setFiltro] = useState("todas")
+  const [carregouTarefas, setCarregouTarefas] = useState(false)
+
+  useEffect(() => {
+    const tarefasSalvas = localStorage.getItem("tarefas")
+
+    if (tarefasSalvas) {
+      setTarefas(JSON.parse(tarefasSalvas))
+    }
+
+    setCarregouTarefas(true)
+  }, [])
+
+  useEffect(() => {
+    if (carregouTarefas) {
+      localStorage.setItem("tarefas", JSON.stringify(tarefas))
+    }
+  }, [tarefas, carregouTarefas])
 
   const concluirTarefa = (id) => {
     setTarefas(
